19ec3571435dd98b3d8c63311999b9c84c8688d0
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2022-09-20 23:14:34 -0300

    Create Servidor.java

---
+++ Servidor.java
@@ -0,0 +1,32 @@
+package socket;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Servidor {
+
+    public Servidor(int port) throws IOException {
+        
+        
+        
+        ServerSocket servidor = new ServerSocket(port);
+        System.out.println("Porta "+ port +" aberta!");
+
+        Socket cliente = servidor.accept();
+        System.out.println("Cliente " +
+                cliente.getInetAddress().getHostAddress() + " adicionado."
+        );
+
+        Scanner s = new Scanner(cliente.getInputStream());
+        while (s.hasNextLine()) {
+            System.out.println(s.nextLine());
+        }
+
+        s.close();
+        servidor.close();
+        cliente.close();
+    }
+
+}
